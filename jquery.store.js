584045246661875e1468f9a3c55172e6e7e680e5
Jonathan Kowalski <jonathankowalski@ymail.com>
2012-05-05 12:11:00 +0200

    stockage local

--- jquery.store.js
+++ jquery.store.js
@@ -1,19 +1,62 @@
 (function($){
 
 	var storage = {
+
+		localStorage: true,
+		sessionEnabled: true,
+
 		init: function(params){
 			this.params = $.extend({
-				env: "debug"
+				env: "debug",
+				fallback: true,
+				verbose: true
 			}, params);
 			if(!window.localStorage){
-				this.local = false;
+				this.localEnabled = false;
+			}
+			if(!window.sessionStorage){
+				this.sessionEnabled = false;
 			}
 			return this;
 		},
 
-		setLocal: function(key, value){},
+		setInLocal: function(key, value){			
+			var keyValues = this.__parseKeyValues(key, value);			
+			if(this.localStorage){				
+				return this.__set(keyValues, 'local');
+			} else if(this.params.fallback) {
+				return this.__setFallback(key, value);
+			} else if(this.params.verbose) {
+				throw ("localStorage is unsupported");
+			}
+			return false;
+		},
 
-		setSession: function(key, value){},
+		__set: function(element, context){
+			for (var key in element){				
+				if(!element.hasOwnProperty(key)){
+					continue;
+				}
+				switch(context){
+					case 'local':
+						this.__addToLocal(key, element[key]);
+						break;
+					case 'session':
+						this.__addToSession(key, element[key]);
+						break;
+				}
+			}			
+		},
+
+		__addToLocal: function(key, value){			
+			window.localStorage[key] = value;	
+		},
+
+		__addToSession: function(key, value){			
+			window.sessionStorage[key] = value;	
+		},
+
+		setInSession: function(key, value){},
 
 		get: function(key, context){},
 
@@ -21,7 +64,21 @@
 
 		flush: function(){},
 
-		__set: function(key, value){}
+		__parseKeyValues: function(key, value){
+			if(value){
+				var obj = {}
+				obj[key] = value;
+				return obj;
+			} else {
+				if(typeof(key) == "object"){
+					return key;
+				}				
+			}
+		},
+
+
+
+		__setFallback: function(key, value){}
 	};
 
 	$.store = storage.init();
