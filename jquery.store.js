c0e429ad15512239a92619db13dcc72271186481
Jonathan Kowalski <jonathankowalski@ymail.com>
2012-05-05 12:59:07 +0200

    ajout au storage et flush

--- jquery.store.js
+++ jquery.store.js
@@ -2,67 +2,103 @@
 
 	var storage = {
 
-		localStorage: true,
-		sessionEnabled: true,
+		contexts: {
+			"local": "local",
+			"session": "session"
+		},
+
+		enabled: {
+			"local": true,
+			"session": true
+		},		
+
+		storageInterface: {
+			"local": window.localStorage,
+			"session": window.sessionStorage
+		},
 
 		init: function(params){
-			this.params = $.extend({
-				env: "debug",
+			this.params = $.extend({				
 				fallback: true,
-				verbose: true
+				verbose: true				
 			}, params);
 			if(!window.localStorage){
-				this.localEnabled = false;
+				this.enabled["local"] = false;
 			}
 			if(!window.sessionStorage){
-				this.sessionEnabled = false;
+				this.enabled["session"] = false;
 			}
 			return this;
 		},
 
-		setInLocal: function(key, value){			
-			var keyValues = this.__parseKeyValues(key, value);			
-			if(this.localStorage){				
-				return this.__set(keyValues, 'local');
-			} else if(this.params.fallback) {
-				return this.__setFallback(key, value);
-			} else if(this.params.verbose) {
-				throw ("localStorage is unsupported");
-			}
+		setInLocal: function(key, value){
+			var value = value || false;
+			return this.__set(key, value, this.contexts.local);
+		},
+
+		setInSession: function(key, value){
+			var value = value || false;
+			return this.__set(key, value, this.contexts.session);
+		},		
+
+		__set: function(key, value, context){
+			var keyValues = this.__parseKeyValues(key, value);
+			if(this.__isSupported(context)){
+				return this.__addObjectIn(keyValues, context);
+			} else if (this.params.fallback) {
+				return this.__addObjectIn(keyValues, 'cookie');
+			} else if (this.params.verbose) {
+				throw (context+"Storage is not supported");
+			}		
 			return false;
 		},
 
-		__set: function(element, context){
+		__isSupported: function(context){
+			return this.enabled[context];
+		},
+
+		__addObjectIn: function(element, context){
 			for (var key in element){				
 				if(!element.hasOwnProperty(key)){
 					continue;
 				}
-				switch(context){
-					case 'local':
-						this.__addToLocal(key, element[key]);
-						break;
-					case 'session':
-						this.__addToSession(key, element[key]);
-						break;
-				}
-			}			
-		},
+				this.addMethods[context](key, element[key]);
+			}	
+			return true;
+		},		
 
-		__addToLocal: function(key, value){			
-			window.localStorage[key] = value;	
-		},
-
-		__addToSession: function(key, value){			
-			window.sessionStorage[key] = value;	
-		},
-
-		setInSession: function(key, value){},
+		addMethods: {
+			"local": function(key, value){			
+				window.localStorage[key] = value;	
+			},
+			"session": function(key, value){			
+				window.sessionStorage[key] = value;	
+			},
+			"cookie":  function(key, value){
+				document.cookie = key+"="+value+"; path=/";
+			}
+		},		
 
 		get: function(key, context){},
 
 		remove: function(key, context){},
 
-		flush: function(){},
+		flush: function(context){
+			var context = context || false;
+			if(!context){
+				this.__clearAllStorages();
+			} else {
+				this.storageInterface[context].clear();
+			}
+		},
+
+		__clearAllStorages: function(){
+			for(var context in this.contexts){
+				if(this.contexts.hasOwnProperty(context)){
+					this.storageInterface[context].clear();
+				}
+			}			
+		},
 
 		__parseKeyValues: function(key, value){
 			if(value){
@@ -74,11 +110,7 @@
 					return key;
 				}				
 			}
-		},
-
-
-
-		__setFallback: function(key, value){}
+		}
 	};
 
 	$.store = storage.init();
